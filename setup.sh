0c3be4b565e138b9d1e77cf17068c62983d00283
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2014-07-18 02:54:24 +0000

    Added Git configuration

--- setup.sh
+++ setup.sh
@@ -1,25 +1,46 @@
 #!/bin/bash 
 
-#Disable ipv-
-echo "#disable ipv6" | sudo tee -a /etc/sysctl.conf
-echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
-echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
-echo "net.ipv6.conf.lo.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
+#Disable IPv6
+#sudo echo "#disable ipv6" | sudo tee -a /etc/sysctl.conf
+#sudo echo "net.ipv6.conf.all.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
+#sudo echo "net.ipv6.conf.default.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
+#sudo echo "net.ipv6.conf.lo.disable_ipv6 = 1" | sudo tee -a /etc/sysctl.conf
 
 #Adding useful repositories
 sudo apt-add-repository -y "deb http://repository.spotify.com stable non-free"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59 
-sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
-#sudo add-apt-repository ppa:ubuntu-wine/ppa -y
-sudo add-apt-repository ppa:ubuntu-wine/ppa ppa:chris-lea/node.js -y
+sudo add-apt-repository ppa:numix/ppa -y
+sudo add-apt-repository ppa:deluge-team/ppa -y && sudo add-apt-repository ppa:webupd8team/atom -y
+sudo add-apt-repository ppa:synapse-core/testing -y && sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y && sudo add-apt-repository ppa:chris-lea/node.js -y
+
+#Setting up Google Chrome
+#
+#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+#sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+#
+
 
 sudo apt-get update
+#sudo apt-get install google-chrome-stable
+sudo apt-get install gdebi git numix-gtk-theme numix-icon-theme-circle typecatcher deluge redshift synapse atom deluge curl zsh smplayer p7zip p7zip-full p7zip-rar ubuntu-restricted-extras sublime-text-installer synaptic spotify-client nodejs -y
+#sudo apt-get install ttf-mscorefonts-installer --quiet -y
 
-sudo apt-get install zsh smplayer firefox ubuntu-restricted-extras sublime-text-installer synaptic spotify-client nodejs -y
+sudo apt-get dist-upgrade -y 
 
 #Setting oh-my-zsh (zsh being an alternative to bash) https://github.com/robbyrussell/oh-my-zsh
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | bash
-sudo chsh -s $(which zsh) $(whoami)
+chsh -s $(which zsh) $(whoami)
+
+# Git configuration
+git config --global user.email "edmeral.aissam@gmail.com"
+git config --global user.name "Aissam BELHACHMI"
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+git config --global alias.st 'status'
+git config --global alias.ci 'commit'
+git config --global alias.co 'checkout'
+git config --global alias.br 'branch'
 
 
 
