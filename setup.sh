163dce4ec5c14f52a6d205a5205511c971fc16af
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2015-05-16 22:21:24 +0100

    Using simple behavior for git push

--- setup.sh
+++ setup.sh
@@ -39,6 +39,7 @@ git config --global alias.ci 'commit'
 git config --global alias.co 'checkout'
 git config --global alias.br 'branch'
 git config core.filemode false
+git config --global push.default simple
 
 # Changing window button layout
 gsettings set org.gnome.shell.overrides button-layout :minimize,close
