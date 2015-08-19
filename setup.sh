beb539240ebb358f1e1fc19d154f74044a4c5582
Aissam BELHACHMI <edmeral.aissam@gmail.com>
2015-08-19 22:09:12 +0100

    filemode --> fileMode

--- setup.sh
+++ setup.sh
@@ -39,7 +39,7 @@ git config --global alias.st 'status'
 git config --global alias.ci 'commit'
 git config --global alias.co 'checkout'
 git config --global alias.br 'branch'
-git config core.filemode false
+git config core.fileMode false
 git config --global push.default simple
 
 # Changing window button layout
